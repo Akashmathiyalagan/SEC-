8f890d02e8647d5fe890624c9d980643181162f9
Akash <166621275+Akashmathiyalagan@users.noreply.github.com>
2025-07-21 07:54:59 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,12 @@
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+
+let N = 5, sum = 0;
+for (let i = 1; i <= N; i++) {
+  sum += i;
+}
+console.log("Sum of first", N, "numbers is:", sum);
